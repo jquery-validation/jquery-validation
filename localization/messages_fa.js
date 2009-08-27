90d4d937c3eb93b65d6aa6bc2afd0d990f88d868
Jörn Zaeffferer <joern.zaefferer@gmail.com>
2009-08-27 07:00:14 +0000

    * Added Persian (FA) localization

---
+++ localization/messages_fa.js
@@ -0,0 +1,27 @@
+/*
+ * validation plugin of JQuery
+ * localize of persian(FA) 
+ * Language: FA(Persian)
+ * Traduzione a cura di Davide Falchetto
+ * E-mail: sinoohe.mkh@gmail.com
+ * Web: http://30noohe.wordpress.com
+ */
+jQuery.extend(jQuery.validator.messages, {
+       required: "تکمیل این فیلد اجباری است.",
+       remote: "لطفا این فیلد را تصحیح کنید.",
+       email: ".لطفا یک ایمیل صحیح وارد کنید",
+       url: "لطفا آدرس صحیح وارد کنید.",
+       date: "لطفا یک تاریخ صحیح وارد کنید",
+       dateISO: "لطفا تاریخ صحیح وارد کنید (ISO).",
+       number: "لطفا عدد صحیح وارد کنید.",
+       digits: "لطفا تنها رقم وارد کنید",
+       creditcard: "لطفا کریدیت کارت صحیح وارد کنید.",
+       equalTo: "لطفا مقدار برابری وارد کنید",
+       accept: "لطفا مقداری وارد کنید که ",
+       maxlength: jQuery.validator.format("لطفا بیشتر از {0} حرف وارد نکنید."),
+       minlength: jQuery.validator.format("لطفا کمتر از {0} حرف وارد نکنید."),
+       rangelength: jQuery.validator.format("لطفا مقداری بین {0} تا {1} حرف وارد کنید."),
+       range: jQuery.validator.format("لطفا مقداری بین {0} تا {1} حرف وارد کنید."),
+       max: jQuery.validator.format("لطفا مقداری کمتر از {0} حرف وارد کنید."),
+       min: jQuery.validator.format("لطفا مقداری بیشتر از {0} حرف وارد کنید.")
+}); 
