18ca28c49916547051dc7a74470cbcadecc662d8
Jörn Zaeffferer <joern.zaefferer@gmail.com>
2010-01-25 18:27:18 +0000

    Fixed four missing semicolons (http://plugins.jquery.com/node/12639)

--- jquery.validate.js
+++ jquery.validate.js
@@ -238,7 +238,7 @@ $.extend($.validator, {
 				this.element(element);
 			// or option elements, check parent select in that case
 			else if (element.parentNode.name in this.submitted)
-				this.element(element.parentNode)
+				this.element(element.parentNode);
 		},
 		highlight: function( element, errorClass, validClass ) {
 			$(element).addClass(errorClass).removeClass(validClass);
@@ -650,7 +650,7 @@ $.extend($.validator, {
 		errorsFor: function(element) {
 			var name = this.idOrName(element);
     		return this.errors().filter(function() {
-				return $(this).attr('for') == name
+				return $(this).attr('for') == name;
 			});
 		},
 		
@@ -861,7 +861,7 @@ $.extend($.validator, {
 		
 		// To support custom messages in metadata ignore rule methods titled "messages"
 		if (rules.messages) {
-			delete rules.messages
+			delete rules.messages;
 		}
 		
 		return rules;
@@ -1136,5 +1136,5 @@ $.format = $.validator.format;
 		triggerEvent: function(type, target) {
 			return this.triggerHandler(type, [$.event.fix({ type: type, target: target })]);
 		}
-	})
+	});
 })(jQuery);
