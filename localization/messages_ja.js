7cf368a98b5743f2188f3c88c1d072409101bd58
Jörn Zaefferer <joern.zaefferer@gmail.com>
2011-05-07 19:15:20 +0200

    iAdded Vietnamese (VI) localization, thanks Ngoc

--- localization/messages_ja.js
+++ localization/messages_ja.js
@@ -1,6 +1,6 @@
 /*
  * Translated default messages for the jQuery validation plugin.
- * Language: JA
+ * Locale: JA (Japanese)
  */
 jQuery.extend(jQuery.validator.messages, {
   required: "このフィールドは必須です。",
