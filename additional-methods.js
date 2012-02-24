759f5e23c0aaa56086b80370a5dc9fe3224b9220
emirpprime <stickmanphotography@gmail.com>
2012-02-24 11:36:44 +0000

    Prevent phone invalidation for ' ' or '-' inclusion
    Spaces are standard notation, hyphens US style, both likely to occur in
    otherwise valid numbers.

--- additional-methods.js
+++ additional-methods.js
@@ -188,11 +188,13 @@ jQuery.validator.addMethod("phoneUS", function(phone_number, element) {
 }, "Please specify a valid phone number");
 
 jQuery.validator.addMethod('phoneUK', function(phone_number, element) {
+phone_number = phone_number.replace(/\s+|-/g,'');
 return this.optional(element) || phone_number.length > 9 &&
 phone_number.match(/^(\(?(0|\+44)[1-9]{1}\d{1,4}?\)?\s?\d{3,4}\s?\d{3,4})$/);
 }, 'Please specify a valid phone number');
 
 jQuery.validator.addMethod('mobileUK', function(phone_number, element) {
+phone_number = phone_number.replace(/\s+|-/g,'');
 return this.optional(element) || phone_number.length > 9 &&
 phone_number.match(/^((0|\+44)7(5|6|7|8|9){1}\d{2}\s?\d{6})$/);
 }, 'Please specify a valid mobile number');
