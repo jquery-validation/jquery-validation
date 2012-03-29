85b719b8d343de90cad737a3ed49aac14c38c63e
Max Lynch <ihasmax@gmail.com>
2012-03-29 11:08:01 -0500

    Fix maxWords method to include the upper bound, not exclude it. Fixes #284

--- test/methods.js
+++ test/methods.js
@@ -527,8 +527,10 @@ test("maxWords", function() {
 	ok( method("hello", 2), "plain text, valid" );
 	ok( method("<b>world</b>", 2), "html, valid" );
 	ok( method("world <br/>", 2), "html, valid" );
-	ok( !method("hello worlds", 2), "plain text, invalid" );
-	ok( !method("<b>hello</b> world", 2), "html, invalid" );
+	ok( method("hello worlds", 2), "plain text, valid" );
+	ok( method("<b>hello</b> world", 2), "html, valid" );
+	ok( !method("hello my world", 2), "plain text, invalid" );
+	ok( !method("<b>hello</b> my world", 2), "html, invalid" );
 });
 
 test("pattern", function() {
