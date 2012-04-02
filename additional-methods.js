dc0a6ef53076b6be272dea9a019a15e00aa61d07
Simon Pioli <simon@simonpioli.com>
2012-04-02 12:19:37 +0100

    Added "Require from Group" method

--- additional-methods.js
+++ additional-methods.js
@@ -338,3 +338,47 @@ jQuery.validator.addMethod("pattern", function(value, element, param) {
     return param.test(value);
 }, "Invalid format.");
 
+//Require one of a group of inputs, should be grouped in a div with a unique class
+jQuery.validator.addMethod("require_from_group", function(value, element, options) {
+    var numberRequired = options[0];
+    var selector = options[1];
+    //Look for our selector within the parent form
+    var validOrNot = $(selector, element.form).filter(function() {
+         // Each field is kept if it has a value
+         return $(this).val();
+         // Set to true if there are enough, else to false
+      }).length >= numberRequired;
+
+    // The elegent part - this element needs to check the others that match the
+    // selector, but we don't want to set off a feedback loop where each element
+    // has to check each other element. It would be like:
+    // Element 1: "I might be valid if you're valid. Are you?"
+    // Element 2: "Let's see. I might be valid if YOU'RE valid. Are you?"
+    // Element 1: "Let's see. I might be valid if YOU'RE valid. Are you?"
+    // ...etc, until we get a "too much recursion" error.
+    //
+    // So instead we
+    //  1) Flag all matching elements as 'currently being validated'
+    //  using jQuery's .data()
+    //  2) Re-run validation on each of them. Since the others are now
+    //     flagged as being in the process, they will skip this section,
+    //     and therefore won't turn around and validate everything else
+    //  3) Once that's done, we remove the 'currently being validated' flag
+    //     from all the elements
+    if(!$(element).data('being_validated')) {
+    var fields = $(selector, element.form);
+    fields.data('being_validated', true);
+    // .valid() means "validate using all applicable rules" (which 
+    // includes this one)
+    fields.valid();
+    fields.data('being_validated', false);
+    }
+    return validOrNot;
+    // {0} below is the 0th item in the options field
+}, jQuery.format("Please choose a memorial."));
+
+/* Example Class Rule for Require from Group
+jQuery.validator.addClassRules("choose-mem", {
+    require_from_group: [1,".choose-mem"]
+});
+*/
