fa19e03df72ea4ff9507d992a0269ea6d3dad712
Simon Pioli <simon@simonpioli.com>
2012-04-03 17:23:42 +0100

    Added "Not Equal to Array" method

--- additional-methods.js
+++ additional-methods.js
@@ -33,6 +33,11 @@
 
 })();
 
+//Not equal to an array of options
+jQuery.validator.addMethod("notEqualToArray", function(value, element, param) {
+    return this.optional(element) || $.inArray(value, param) == -1;
+}, jQuery.validator.format("Please choose another option."));
+
 jQuery.validator.addMethod("letterswithbasicpunc", function(value, element) {
 	return this.optional(element) || /^[a-z-.,()'\"\s]+$/i.test(value);
 }, "Letters or punctuation only please");
