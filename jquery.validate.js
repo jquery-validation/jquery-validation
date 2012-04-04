e036239746aabc35bb00684d4f15da19c5710486
Max Lynch <ihasmax@gmail.com>
2012-04-04 11:43:10 -0500

    required attribute fix for non-html5 browsers. Fixes #301

--- jquery.validate.js
+++ jquery.validate.js
@@ -837,12 +837,23 @@ $.extend($.validator, {
 
 		for (var method in $.validator.methods) {
 			var value;
-			// If .prop exists (jQuery >= 1.6), use it to get true/false for required
-			if (method === 'required' && typeof $.fn.prop === 'function') {
-				value = $element.prop(method);
+
+			// support for <input required> in both html5 and older browsers
+			if (method === 'required') {
+				value = $element.get(0).getAttribute(method);
+				// Some browsers return an empty string for the required attribute
+				// and non-HTML5 browsers might have required="" markup
+				if (value === "") {
+					value = true;
+				} else if (value === "false") {
+					value = false;
+				}
+				// force non-HTML5 browsers to return bool
+				value = !!value;
 			} else {
 				value = $element.attr(method);
 			}
+
 			if (value) {
 				rules[method] = value;
 			} else if ($element[0].getAttribute("type") === method) {
