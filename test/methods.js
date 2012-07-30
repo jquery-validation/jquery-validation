3b95259db9697273f08af5b67f2ea87c5b233244
g1smd <g1smd.email@gmail.com>
2012-07-30 09:21:51 +0000

    Tests for UK mobile numbers.

--- test/methods.js
+++ test/methods.js
@@ -554,11 +554,16 @@ test("phone (us)", function() {
 
 test("mobileUK", function() {
 	var method = methodTest("mobileUK");
-	ok( method( "07434234323" ), "Valid UK Mobile Number" );
-	ok( method( "07011111111" ), "Valid UK Mobile Number" );
-	ok( method( "+447011111111" ), "Valid UK Mobile Number" );
-	ok( !method( "07334234323" ), "Invalid UK Mobile Number" );
-	ok( !method( "11111111111" ), "Invalid UK Mobile Number" );
+	ok( method( "07734234323" ), "Valid UK Mobile Number" );
+	ok( method( "+447734234323" ), "Valid UK Mobile Number" );
+	ok( !method( "0753423432" ), "Invalid UK Mobile Number" );
+	ok( !method( "07604234323" ), "Invalid UK Mobile Number" );
+	ok( !method( "077342343234" ), "Invalid UK Mobile Number" );
+	ok( !method( "044342343234" ), "Invalid UK Mobile Number" );
+	ok( !method( "+44753423432" ), "Invalid UK Mobile Number" );
+	ok( !method( "+447604234323" ), "Invalid UK Mobile Number" );
+	ok( !method( "+4477342343234" ), "Invalid UK Mobile Number" );
+	ok( !method( "+4444342343234" ), "Invalid UK Mobile Number" );
 });
 
 test("dateITA", function() {
