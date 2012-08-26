11222c8f17a90d815ff73c5f9a0393047a1fa0a9
g1smd <g1smd.email@gmail.com>
2012-08-26 12:27:27 +0000

    One more non-valid GB mobile phone number for testing.

--- test/methods.js
+++ test/methods.js
@@ -556,6 +556,7 @@ test("mobileUK", function() {
 	var method = methodTest("mobileUK");
 	ok( method( "07734234323" ), "Valid UK Mobile Number" );
 	ok( method( "+447734234323" ), "Valid UK Mobile Number" );
+	ok( !method( "07034234323" ), "Invalid UK Mobile Number" );
 	ok( !method( "0753423432" ), "Invalid UK Mobile Number" );
 	ok( !method( "07604234323" ), "Invalid UK Mobile Number" );
 	ok( !method( "077342343234" ), "Invalid UK Mobile Number" );
