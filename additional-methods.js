40fe2101d5d03da8e1965a9faa7998958646557c
g1smd <g1smd.email@gmail.com>
2012-09-16 14:20:08 +0000

    Minor edit to comment.

--- additional-methods.js
+++ additional-methods.js
@@ -193,14 +193,14 @@ jQuery.validator.addMethod('phoneUK', function(phone_number, element) {
 }, 'Please specify a valid phone number');
 
 jQuery.validator.addMethod('mobileUK', function(phone_number, element) {
-	phone_number = phone_number.replace(/\s+|-/g,'');
+	phone_number = phone_number.replace(/\(|\)|\s+|-/g,'');
 	return this.optional(element) || phone_number.length > 9 &&
 		phone_number.match(/^(?:(?:(?:00\s?|\+)44\s?|0)7(?:[45789]\d{2}|624)\s?\d{3}\s?\d{3})$/);
 }, 'Please specify a valid mobile number');
 
 //Matches UK landline + mobile, accepting only 01-3 for landline or 07 for mobile to exclude many premium numbers
 jQuery.validator.addMethod('phonesUK', function(phone_number, element) {
-	phone_number = phone_number.replace(/\s+|-/g,'');
+	phone_number = phone_number.replace(/\(|\)|\s+|-/g,'');
 	return this.optional(element) || phone_number.length > 9 &&
 		phone_number.match(/^(?:(?:(?:00\s?|\+)44\s?|0)(?:1\d{8,9}|[23]\d{9}|7(?:[45789]\d{8}|624\d{6})))$/);
 }, 'Please specify a valid uk phone number');
@@ -208,7 +208,7 @@ jQuery.validator.addMethod('phonesUK', function(phone_number, element) {
 //  Compare original input with this RegEx pattern:
 //   ^\(?(?:(?:00\)?[\s-]?\(?|\+)(44)\)?[\s-]?\(?(?:0\)?[\s-]?\(?)?|0)([1-9]\d{1,4}\)?[\s-\d]+)$
 //  Extract $1 and set $prefix to '+44<space>' if $1 is '44', otherwise set $prefix to '0'
-//  Extract $2 and remove spaces and parentheses. Phone number is combined $prefix and $2.
+//  Extract $2 and remove hyphens, spaces and parentheses. Phone number is combined $prefix and $2.
 // A number of very detailed GB telephone number RegEx patterns can also be found at:
 // http://www.aa-asterisk.org.uk/index.php/Regular_Expressions_for_Validating_and_Formatting_UK_Telephone_Numbers
 
