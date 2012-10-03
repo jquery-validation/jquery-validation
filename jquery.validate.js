5934f4d4509b900594b40eb4399446dd46debf36
Jörn Zaefferer <joern.zaefferer@gmail.com>
2012-10-03 18:41:25 +0200

    Undo fix for #244. Fixes #521 - E-mail validation fires immediately when text is in the field.

--- jquery.validate.js
+++ jquery.validate.js
@@ -236,7 +236,7 @@ $.extend($.validator, {
 		onkeyup: function(element, event) {
 			if ( event.which === 9 && this.elementValue(element) === '' ) {
 				return;
-			} else if ( element.name in this.submitted || element === this.lastActive ) {
+			} else if ( element.name in this.submitted || element === this.lastElement ) {
 				this.element(element);
 			}
 		},
