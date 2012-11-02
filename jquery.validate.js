9a28ba7af04a1dbd23a9ca95dffe63e35886f9af
niahmiah <ian.patton@gmail.com>
2012-11-02 17:49:24 -0300

    Update jquery.validate.js
    Enable classes for additional-methods

--- jquery.validate.js
+++ jquery.validate.js
@@ -978,7 +978,7 @@ $.extend($.validator, {
 	addMethod: function(name, method, message) {
 		$.validator.methods[name] = method;
 		$.validator.messages[name] = message !== undefined ? message : $.validator.messages[name];
-		if (method.length < 3) {
+		if (method.length < 4) {
 			$.validator.addClassRules(name, $.validator.normalizeRule(name));
 		}
 	},
