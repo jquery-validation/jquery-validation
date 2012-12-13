7c91dcc4ae61ffb4c9ce51c0903987cccff1d944
Yoav Ram <yoavram+github@gmail.com>
2012-12-13 16:16:38 +0200

    changed indentation

--- localization/messages_he.js
+++ localization/messages_he.js
@@ -19,5 +19,6 @@ jQuery.extend(jQuery.validator.messages, {
 	rangelength: jQuery.validator.format("נא למלא ערך בין {0} ל- {1} תווים"),
 	range: jQuery.validator.format("נא למלא ערך בין {0} ל- {1}"),
 	max: jQuery.validator.format("נא למלא ערך קטן או שווה ל- {0}"),
-	min: jQuery.validator.format("נא למלא ערך גדול או שווה ל- {0}")
+	min: jQuery.validator.format("נא למלא ערך גדול או שווה ל- {0}"),
+	greaterThan: jQuery.validator.format('צריך להיות מאוחר יותר מאשר {0}')
 });
