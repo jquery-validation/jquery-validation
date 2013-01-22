315189301a96d347fc30fb593e737c329de5e94a
Jörn Zaefferer <joern.zaefferer@gmail.com>
2013-01-22 22:21:43 +0100

    Add plugin manifest and update headers to just MIT license, drop unnecessary dual-licensing (like jQuery).

--- additional-methods.js
+++ additional-methods.js
@@ -4,11 +4,9 @@
  * http://bassistance.de/jquery-plugins/jquery-plugin-validation/
  * http://docs.jquery.com/Plugins/Validation
  *
- * Copyright (c) 2006 - 2011 Jörn Zaefferer
- *
- * Dual licensed under the MIT and GPL licenses:
+ * Copyright 2013 Jörn Zaefferer
+ * Released under the MIT license:
  *   http://www.opensource.org/licenses/mit-license.php
- *   http://www.gnu.org/licenses/gpl.html
  */
 
 (function() {
