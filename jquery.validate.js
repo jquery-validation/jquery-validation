9a6372c0025d1cfbbd5ac7c8073698f069e38332
John-Philip Johansson <seriema@gmail.com>
2013-01-23 19:32:03 +0100

    Bool conversion not needed in attrConv(), removed.

--- jquery.validate.js
+++ jquery.validate.js
@@ -22,12 +22,6 @@ var attrConv = function($element, property) {
 	if (!isNaN(asInt)) {			
 		return asInt;
 	}
-	if (value === "false") {
-		return false;
-	}
-	if (value === "true") {
-		return true;
-	}
 
 	return value;
 };
