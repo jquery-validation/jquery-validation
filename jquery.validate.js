cfe4dba5371625dd7848ab066c08e3561a7fa578
Luca Fagioli <l.fagioli@tiscali.it>
2013-02-01 10:43:06 +0100

    Update jquery.validate.js
    minor fix.

--- jquery.validate.js
+++ jquery.validate.js
@@ -355,7 +355,7 @@ $.extend($.validator, {
 		checkForm: function() {
 			this.prepareForm();
 			for ( var i = 0, elements = (this.currentElements = this.elements()); elements[i]; i++ ) {
-				if (this.findByName( elements[i].name ).length != undefined && this.findByName( elements[i].name ).length > 1) {
+				if (this.findByName( elements[i].name ).length !== undefined && this.findByName( elements[i].name ).length > 1) {
 					for (var cnt = 0; cnt < this.findByName( elements[i].name ).length; cnt++) {
 						this.check( this.findByName( elements[i].name )[cnt] );
 					}
