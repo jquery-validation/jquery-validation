b92669ef9517d265a9e0ef23c285ca577a1eb550
monteiz <l.fagioli@tiscali.it>
2013-02-01 10:28:12 +0100

    Update jquery.validate.js
    Based on this SO answer:
    
        http://stackoverflow.com/a/12892859/636561
    
    this update allows to validate an array of input text elements (not only the first element of the array), taking into count the "ignore" setting.

--- jquery.validate.js
+++ jquery.validate.js
@@ -355,7 +355,13 @@ $.extend($.validator, {
 		checkForm: function() {
 			this.prepareForm();
 			for ( var i = 0, elements = (this.currentElements = this.elements()); elements[i]; i++ ) {
-				this.check( elements[i] );
+				if (this.findByName( elements[i].name ).length != undefined && this.findByName( elements[i].name ).length > 1) {
+					for (var cnt = 0; cnt < this.findByName( elements[i].name ).length; cnt++) {
+						this.check( this.findByName( elements[i].name )[cnt] );
+					}
+				} else {
+					this.check( elements[i] );
+				}
 			}
 			return this.valid();
 		},
