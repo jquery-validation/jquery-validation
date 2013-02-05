ff26f320c63830080ee22be6466e62c8ef9a23b6
Jörn Zaefferer <joern.zaefferer@gmail.com>
2013-02-05 10:19:27 +0100

    Fix post-release version

--- jquery.validate.js
+++ jquery.validate.js
@@ -1,5 +1,5 @@
 /**
- * jQuery Validation Plugin 1.12.0
+ * jQuery Validation Plugin 1.12.0pre
  *
  * http://bassistance.de/jquery-plugins/jquery-plugin-validation/
  * http://docs.jquery.com/Plugins/Validation
