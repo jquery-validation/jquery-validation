22631a176080a07273c384d40e89060e051f0492
James Thompson <james@ascensor.co.uk>
2013-03-12 15:50:35 +0000

    Modified the UK postcode method

--- additional-methods.js
+++ additional-methods.js
@@ -390,7 +390,7 @@ jQuery.validator.addMethod('phonesUK', function(phone_number, element) {
 // http://www.aa-asterisk.org.uk/index.php/Regular_Expressions_for_Validating_and_Formatting_GB_Telephone_Numbers
 
 // Matches UK postcode. 
-jQuery.validator.addMethod('postcodeUK', function(postcode, element) {	
+jQuery.validator.addMethod('postcodeUK', function(value, element) {	
 	return this.optional(element) || /^((([A-PR-UWYZ][0-9])|([A-PR-UWYZ][0-9][0-9])|([A-PR-UWYZ][A-HK-Y][0-9])|([A-PR-UWYZ][A-HK-Y][0-9][0-9])|([A-PR-UWYZ][0-9][A-HJKSTUW])|([A-PR-UWYZ][A-HK-Y][0-9][ABEHMNPRVWXY]))\s?([0-9][ABD-HJLNP-UW-Z]{2})|(GIR)\s?(0AA))$/i.test(value);
 }, 'Please specify a valid postcode');
 
