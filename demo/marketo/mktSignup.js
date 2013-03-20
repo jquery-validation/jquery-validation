8250adb140aea714019b8d074f5c1ad3934a3925
Jörn Zaefferer <joern.zaefferer@gmail.com>
2013-03-20 15:40:10 +0100

    Replace most usage of PHP with mockjax handlers. Do some demo cleanup as well, update to newer masked-input plugin. Keep captcha demo in PHP. Fixes #662

--- demo/marketo/mktSignup.js
+++ demo/marketo/mktSignup.js
@@ -1,5 +1,17 @@
- $(document).ready(function(){
- 	
+$(document).ready(function(){
+	$.mockjax({
+		url: "emails.action",
+		response: function(settings) {
+			var email = settings.data.email,
+				emails = ["glen@marketo.com", "george@bush.gov", "me@god.com", "aboutface@cooper.com", "steam@valve.com", "bill@gates.com"];
+			this.responseText = "true";
+			if ( $.inArray( email, emails ) !== -1 ) {
+				this.responseText = "false";
+			}
+		},
+		responseTime: 500
+	});
+
 	jQuery.validator.addMethod("password", function( value, element ) {
 		var result = this.optional(element) || value.length >= 6 && /\d/.test(value) && /[a-z]/i.test(value);
 		if (!result) {
@@ -13,18 +25,18 @@
 		}
 		return result;
 	}, "Your password must be at least 6 characters long and contain at least one number and one character.");
-	
+
 	// a custom method making the default value for companyurl ("http://") invalid, without displaying the "invalid url" message
 	jQuery.validator.addMethod("defaultInvalid", function(value, element) {
 		return value != element.defaultValue;
 	}, "");
-	
+
 	jQuery.validator.addMethod("billingRequired", function(value, element) {
 		if ($("#bill_to_co").is(":checked"))
 			return $(element).parents(".subTable").length;
 		return !this.optional(element);
 	}, "");
-	
+
 	jQuery.validator.messages.required = "";
 	$("form").validate({
 		invalidHandler: function(e, validator) {
@@ -47,24 +59,20 @@
 		messages: {
 			password2: {
 				required: " ",
-				equalTo: "Please enter the same password as above"	
+				equalTo: "Please enter the same password as above"
 			},
 			email: {
 				required: " ",
 				email: "Please enter a valid email address, example: you@yourdomain.com",
-				remote: jQuery.validator.format("{0} is already taken, please enter a different address.")	
+				remote: jQuery.validator.format("{0} is already taken, please enter a different address.")
 			}
 		},
 		debug:true
 	});
-	
+
   $(".resize").vjustify();
   $("div.buttonSubmit").hoverClass("buttonSubmitHover");
 
-  if ($.browser.safari) {
-    $("body").addClass("safari");
-  }
-  
   $("input.phone").mask("(999) 999-9999");
   $("input.zipcode").mask("99999");
   var creditcard = $("#creditcard").mask("9999 9999 9999 9999");
