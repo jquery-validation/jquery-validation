9af8461b68c610c0ef3a68f68c4707befefb5cc6
jameslouiz <james@wellmadedesigns.com>
2013-03-22 00:02:21 +0000

    Added Currency Validation

--- test/methods.js
+++ test/methods.js
@@ -951,7 +951,6 @@ test("currency", function() { // Works with any symbol
 	ok( method( "£9,999.9" ), "Valid currency" );
 	ok( method( "£9,999.99" ), "Valid currency" );
 	ok( method( "£9,999,999.9" ), "Valid currency" );
-
 	ok(!method( "£9," ), "Invalid currency" );
 	ok(!method( "£9,99.99" ), "Invalid currency" );
 	ok(!method( "£9," ), "Invalid currency" );
