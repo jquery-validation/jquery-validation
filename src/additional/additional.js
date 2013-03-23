e22e5080d3629a1cfb43e3b9b712ea986e6be78e
Nick Schonning <nschonni@gmail.com>
2013-03-22 20:19:38 -0400

    Split out phoneNL

--- src/additional/additional.js
+++ src/additional/additional.js
@@ -33,13 +33,6 @@
 
 }());
 
-/**
- * Dutch phone numbers have 10 digits (or 11 and start with +31).
- */
-jQuery.validator.addMethod("phoneNL", function(value, element) {
-	return this.optional(element) || /^((\+|00(\s|\s?\-\s?)?)31(\s|\s?\-\s?)?(\(0\)[\-\s]?)?|0)[1-9]((\s|\s?\-\s?)?[0-9]){8}$/.test(value);
-}, "Please specify a valid phone number.");
-
 jQuery.validator.addMethod("mobileNL", function(value, element) {
 	return this.optional(element) || /^((\+|00(\s|\s?\-\s?)?)31(\s|\s?\-\s?)?(\(0\)[\-\s]?)?|0)6((\s|\s?\-\s?)?[0-9]){8}$/.test(value);
 }, "Please specify a valid mobile number");
