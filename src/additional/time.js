3a2a81d92a2ec14bd322002c3ef569bfe03de410
Nick Schonning <nschonni@gmail.com>
2013-03-22 20:23:12 -0400

    Split out time

---
+++ src/additional/time.js
@@ -0,0 +1,3 @@
+jQuery.validator.addMethod("time", function(value, element) {
+	return this.optional(element) || /^([01]\d|2[0-3])(:[0-5]\d){1,2}$/.test(value);
+}, "Please enter a valid time, between 00:00 and 23:59");
