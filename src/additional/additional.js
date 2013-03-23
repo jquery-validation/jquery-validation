1e0787f2ea37e7ada8647d54ee580a4d90e6d4eb
Nick Schonning <nschonni@gmail.com>
2013-03-22 20:32:27 -0400

    Split out pattern

--- src/additional/additional.js
+++ src/additional/additional.js
@@ -33,30 +33,6 @@
 
 }());
 
-/**
-* Return true if the field value matches the given format RegExp
-*
-* @example jQuery.validator.methods.pattern("AR1004",element,/^AR\d{4}$/)
-* @result true
-*
-* @example jQuery.validator.methods.pattern("BR1004",element,/^AR\d{4}$/)
-* @result false
-*
-* @name jQuery.validator.methods.pattern
-* @type Boolean
-* @cat Plugins/Validate/Methods
-*/
-jQuery.validator.addMethod("pattern", function(value, element, param) {
-	if (this.optional(element)) {
-		return true;
-	}
-	if (typeof param === 'string') {
-		param = new RegExp('^(?:' + param + ')$');
-	}
-	return param.test(value);
-}, "Invalid format.");
-
-
 /*
  * Lets you say "at least X inputs that match selector Y must be filled."
  *
