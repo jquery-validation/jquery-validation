2317df28a8782fb787e140731f98e4020411732e
Nick Schonning <nschonni@gmail.com>
2013-03-22 20:29:15 -0400

    Split out strippedminlength

---
+++ src/additional/strippedminlength.js
@@ -0,0 +1,4 @@
+// TODO check if value starts with <, otherwise don't try stripping anything
+jQuery.validator.addMethod("strippedminlength", function(value, element, param) {
+	return jQuery(value).text().length >= param;
+}, jQuery.validator.format("Please enter at least {0} characters"));
