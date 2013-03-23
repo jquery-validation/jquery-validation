627f456903601617cd970269029b69aa20b7afe8
Nick Schonning <nschonni@gmail.com>
2013-03-22 20:24:02 -0400

    Split out time12h

--- src/additional/additional.js
+++ src/additional/additional.js
@@ -33,10 +33,6 @@
 
 }());
 
-jQuery.validator.addMethod("time12h", function(value, element) {
-	return this.optional(element) || /^((0?[1-9]|1[012])(:[0-5]\d){1,2}(\ ?[AP]M))$/i.test(value);
-}, "Please enter a valid time in 12-hour am/pm format");
-
 /**
  * matches US phone number format
  *
