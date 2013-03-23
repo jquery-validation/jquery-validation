c1a87f17fc09d6a4789058b43a3bc3cdbfc5c6c6
Nick Schonning <nschonni@gmail.com>
2013-03-22 20:32:54 -0400

    Split out require_from_group

--- src/additional/additional.js
+++ src/additional/additional.js
@@ -34,36 +34,6 @@
 }());
 
 /*
- * Lets you say "at least X inputs that match selector Y must be filled."
- *
- * The end result is that neither of these inputs:
- *
- *  <input class="productinfo" name="partnumber">
- *  <input class="productinfo" name="description">
- *
- *  ...will validate unless at least one of them is filled.
- *
- * partnumber:  {require_from_group: [1,".productinfo"]},
- * description: {require_from_group: [1,".productinfo"]}
- *
- */
-jQuery.validator.addMethod("require_from_group", function(value, element, options) {
-	var validator = this;
-	var selector = options[1];
-	var validOrNot = $(selector, element.form).filter(function() {
-		return validator.elementValue(this);
-	}).length >= options[0];
-
-	if(!$(element).data('being_validated')) {
-		var fields = $(selector, element.form);
-		fields.data('being_validated', true);
-		fields.valid();
-		fields.data('being_validated', false);
-	}
-	return validOrNot;
-}, jQuery.format("Please fill at least {0} of these fields."));
-
-/*
  * Lets you say "either at least X inputs that match selector Y must be filled,
  * OR they must all be skipped (left blank)."
  *
