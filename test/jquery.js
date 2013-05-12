72f179f1c9f786196b0665dce5cd7403c0250fec
Jörn Zaefferer <joern.zaefferer@gmail.com>
2013-05-12 15:03:20 +0200

    Update demos and tests to jQuery 1.9.1

--- test/jquery.js
+++ test/jquery.js
@@ -4,7 +4,7 @@ var parts = document.location.search.slice( 1 ).split( "&" ),
 	length = parts.length,
 	i = 0,
 	current,
-	version = "1.9.0",
+	version = "1.9.1",
 	file = "http://code.jquery.com/jquery-git.js";
 
 for ( ; i < length; i++ ) {
