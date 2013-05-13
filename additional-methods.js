d6df4979a270ed0f8ed61ac7b8f612ed45a730d8
rstaib <rstaib@derklang.com>
2013-05-13 22:08:21 +0200

    Add validation method for BIC

--- additional-methods.js
+++ additional-methods.js
@@ -276,6 +276,23 @@ jQuery.validator.addMethod("iban", function(value, element) {
 	return cRest === 1;
 }, "Please specify a valid IBAN");
 
+/**
+ * BIC is the business identifier code (ISO 9362). This BIC check is not a guarantee for authenticity.
+ *
+ * BIC pattern: BBBBCCLLbbb (8 or 11 characters long; bbb is optional)
+ *
+ * BIC definition in detail:
+ * - First 4 characters - bank code (only letters)
+ * - Next 2 characters - ISO 3166-1 alpha-2 country code (only letters)
+ * - Next 2 characters - location code (letters and digits)
+ *   a. shall not start with '0' or '1'
+ *   b. second character must be a letter ('O' is not allowed) or one of the following digits ('0' for test (therefore not allowed), '1' for passive participant and '2' for active participant)
+ * - Last 3 characters - branch code, optional (shall not start with 'X' except in case of 'XXX' for primary office) (letters and digits)
+ */
+jQuery.validator.addMethod("bic", function(value, element) {
+    return this.optional( element ) || /^([A-Z]{6}[A-Z2-9]{1}[A-NP-Z1-2]{1})(X{3}|[A-WY-Z0-9]{1}[A-Z0-9]{2})?$/.test( value );
+}, "Please specify a valid BIC code");
+
 jQuery.validator.addMethod("dateNL", function(value, element) {
 	return this.optional(element) || /^(0?[1-9]|[12]\d|3[01])[\.\/\-](0?[1-9]|1[012])[\.\/\-]([12]\d)?(\d\d)$/.test(value);
 }, "Please enter a correct date");
