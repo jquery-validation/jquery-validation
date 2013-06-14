97bdb7ac72fe1a737b8bba7af56b2b027464de82
Sérgio Rafael Siqueira <sergiorsiqueira9@gmail.com>
2013-06-14 18:22:51 -0300

    Update messages_pt_BR.js

--- localization/messages_pt_BR.js
+++ localization/messages_pt_BR.js
@@ -7,7 +7,7 @@
 	$.extend($.validator.messages, {
 		required: "Este campo &eacute; requerido.",
 		remote: "Por favor, corrija este campo.",
-		email: "Por favor, forne&ccedil;a um endere&ccedil;o eletr&ocirc;nico v&aacute;lido.",
+		email: "Por favor, forne&ccedil;a um endere&ccedil;o de email v&aacute;lido.",
 		url: "Por favor, forne&ccedil;a uma URL v&aacute;lida.",
 		date: "Por favor, forne&ccedil;a uma data v&aacute;lida.",
 		dateISO: "Por favor, forne&ccedil;a uma data v&aacute;lida (ISO).",
