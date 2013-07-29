e09c41a897b693cb48a5ebd101e965a518a4451c
Alfonso Martín <acmartin@indra.es>
2013-07-29 22:34:32 +0200

    NIF, NIE and CIF numbers validation:
    * cifES.js refactorized by 'nschonni'

--- src/additional/cifES.js
+++ src/additional/cifES.js
@@ -1,46 +1,36 @@
-jQuery.validator.addMethod("cifES",function(value, element) {
-
+jQuery.validator.addMethod( "cifES", function ( value, element ) {
 	"use strict";
-
-	var n = 0,
-		suma = 0,
+ 
+	var n,
+		sum,
 		num = [],
 		CIF = value.toUpperCase();
-
-
+ 
 	// Basic format test 
-	if (!CIF.match('((^[A-Z]{1}[0-9]{7}[A-Z0-9]{1}$|^[T]{1}[A-Z0-9]{8}$)|^[0-9]{8}[A-Z]{1}$)')) {
+	if ( !CIF.match( '((^[A-Z]{1}[0-9]{7}[A-Z0-9]{1}$|^[T]{1}[A-Z0-9]{8}$)|^[0-9]{8}[A-Z]{1}$)' ) ) {
 		return false;
 	}
 	
-
-	for (var i = 0; i < 9; i ++) {
-		num[i] = CIF.substr(i, 1);
+	for ( var i = 0; i < 9; i++ ) {
+		num[i] = parseInt( CIF.charAt( i ), 10 );
 	}
-
+ 
 	// Algorithm for checking CIF codes
-	suma = parseInt(num[2],10) + parseInt(num[4],10) + parseInt(num[6],10);
-	for (var temp1=0, temp2=0, count = 1; count < 8; count += 2){
-		temp1 = 2 * parseInt(num[count],10);
-		temp1 += '';
-		temp1 = temp1.substring(0,1);
-		temp2 = 2 * parseInt(num[count],10);
-		temp2 += '';
-		temp2 = temp2.substring(1,2);
-		if (temp2 === '') {
-			temp2 = '0';
-		}
-
-		suma += (parseInt(temp1,10) + parseInt(temp2,10));
+	sum = num[ 2 ] + num[ 4 ] + num[ 6 ];
+	for ( var count = 1; count < 8; count += 2 ) {
+		var tmp = ( 2 * num[ count ] ).toString(),
+			secondDigit = tmp.charAt( 1 );
+		
+		sum += parseInt( tmp.charAt( 0 ), 10 ) + ( secondDigit === '' ? 0 : parseInt( secondDigit, 10 ) );
 	}
-	suma += '';
-	n = 10 - parseInt(suma.substring(suma.length-1, suma.length),10);
-
-	if (/^[ABCDEFGHJNPQRSUVW]{1}/.test(CIF)){
-		CIF = n + '';
-		return (num[8] === String.fromCharCode(64 + n) || num[8] === CIF.substring(CIF.length-1, CIF.length));
+	
+	if ( /^[ABCDEFGHJNPQRSUVW]{1}/.test( CIF ) ) {
+		sum += '';
+		n = 10 - parseInt( sum.charAt( sum.length - 1 ), 10 );
+		CIF += n;
+		return ( num[ 8 ].toString() === String.fromCharCode( 64 + n ) || num[ 8 ].toString() === CIF.charAt( CIF.length - 1 ) );
 	}
-
+ 
 	return false;
-
-}, "Please specify a valid CIF number.");
+ 
+}, "Please specify a valid CIF number." );
