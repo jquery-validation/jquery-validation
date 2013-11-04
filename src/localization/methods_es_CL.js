d1e5f63f1d9b81a9e5285943ea8e3a09cb2695c7
Max Toro <maxtoroq@gmail.com>
2013-11-04 13:37:01 -0300

    number validator

--- src/localization/methods_es_CL.js
+++ src/localization/methods_es_CL.js
@@ -5,5 +5,8 @@
 jQuery.extend(jQuery.validator.methods, {
 	date: function(value, element) {
 		return this.optional(element) || /^\d\d?\-\d\d?\-\d\d\d?\d?$/.test(value);
+	},
+	number: function (value, element) {
+		return this.optional(element) || /^-?(?:\d+|\d{1,3}(?:\.\d{3})+)(?:,\d+)?$/.test(value);
 	}
 });
