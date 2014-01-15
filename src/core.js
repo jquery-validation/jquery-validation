29dbc788846ee642864d270d0a00efe55e46a10f
Nick Schonning <nschonni@gmail.com>
2014-01-15 17:54:39 -0500

    JSCS: Stick colons to object property names
    The `{url: param}"` seems like an odd comparitor

--- src/core.js
+++ src/core.js
@@ -1177,7 +1177,7 @@ $.extend($.validator, {
 			previous.originalMessage = this.settings.messages[element.name].remote;
 			this.settings.messages[element.name].remote = previous.message;
 
-			param = typeof param === "string" && {url:param} || param;
+			param = typeof param === "string" && {url: param} || param;
 
 			if ( previous.old === value ) {
 				return previous.valid;
