b258647540a40343fdd6e29a24fe26408bd53192
pylover <vahid.mardani@gmail.com>
2014-03-18 03:32:14 +0330

    Additionals: Add persian date method
    Also add localization for minWords and maxWords in persian.
    
    Closes #1038

--- test/methods.js
+++ test/methods.js
@@ -603,6 +603,27 @@ test("dateITA", function() {
 	ok(!method( "01/01/199" ), "Invalid date ITA" );
 });
 
+test("dateFA", function() {
+	var method = methodTest("dateFA");
+
+	ok( method( "1342/12/29" ), "Valid date FA" );
+	ok( method( "1342/12/30" ), "Valid date FA" );
+	ok( method( "1361/6/31" ), "Valid date FA" );
+	ok( method( "1321/11/30" ), "Valid date FA" );
+	ok( method( "1361/1/1" ), "Valid date FA" );
+	ok( method( "1020/3/3" ), "Valid date FA" );
+	ok( method( "1020/03/3" ), "Valid date FA" );
+	ok( method( "1020/3/03" ), "Valid date FA" );
+	ok( method( "1020/03/03" ), "Valid date FA" );
+	ok( method( "1001/7/30" ), "Valid date FA" );
+
+	ok(!method( "1000/1/32" ), "Invalid date FA" );
+	ok(!method( "1323/12/31" ), "Invalid date FA" );
+	ok(!method( "1361/0/11" ), "Invalid date FA" );
+	ok(!method( "63/4/4" ), "Invalid date FA" );
+	ok(!method( "15/6/1361" ), "Invalid date FA" );
+});
+
 test("iban", function() {
 	var method = methodTest("iban");
 	ok( method( "NL20INGB0001234567"), "Valid IBAN");
