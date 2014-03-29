d64452afc8865a76491019ebda3d47f4ca7d4720
Thiago Lagden <lagden@gmail.com>
2014-03-29 14:18:22 -0300

    Add UMD (Universal Module Definition) patterns

--- src/core.js
+++ src/core.js
@@ -8,7 +8,20 @@
  *   http://www.opensource.org/licenses/mit-license.php
  */
 
-(function($) {
+// UMD (Universal Module Definition) patterns for JavaScript modules that work everywhere.
+// https://github.com/umdjs/umd/blob/master/jqueryPluginCommonjs.js
+(function(factory) {
+	if (typeof define === 'function' && define.amd) {
+		// AMD. Register as an anonymous module.
+		define(['jquery'], factory);
+	} else if (typeof exports === 'object') {
+		// Node/CommonJS
+		factory(require('jquery'));
+	} else {
+		// Browser globals
+		factory(jQuery);
+	}
+}(function($) {
 
 $.extend($.fn, {
 	// http://jqueryvalidation.org/validate/
@@ -1239,4 +1252,4 @@ $.format = function deprecated() {
 	throw "$.format has been deprecated. Please use $.validator.format instead.";
 };
 
-}(jQuery));
+}));
