dc03b5c6b0eabe92cff8aa735167f70b4f8f2991
Jörn Zaefferer <joern.zaefferer@gmail.com>
2014-03-31 19:45:49 +0200

    Build: Adopt jquery-release for releases

---
+++ build/release.js
@@ -0,0 +1,31 @@
+/*jshint node:true */
+module.exports = function( Release ) {
+
+function today() {
+	return new Date().toISOString().replace(/T.+/, "");
+}
+
+Release.define({
+	issueTracker: "github",
+	changelogShell: function() {
+		return Release.newVersion + " / " + today() + "\n==================\n";
+	},
+
+	generateArtifacts: function( done ) {
+		Release.exec( "grunt release", "Grunt command failed" );
+		done([
+			"dist/additional-methods.js",
+			"dist/additional-methods.min.js",
+			"dist/jquery.validate.js",
+			"dist/jquery.validate.min.js"
+		]);
+	},
+
+	// disable CDN publishing
+	cdnPublish: false,
+
+	// disable authors check
+	_checkAuthorsTxt: function() {}
+});
+
+};
