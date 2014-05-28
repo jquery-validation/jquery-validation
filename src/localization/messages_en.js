9c3a2dd60dc2c6e04751ffffa5606d7001bd5d14
Mustafa Uysal <m_uysl@hotmail.com>
2014-05-28 10:19:41 +0300

    Update messages_en.js

--- src/localization/messages_en.js
+++ src/localization/messages_en.js
@@ -3,7 +3,7 @@
  * Locale: EN
  */
 $.extend($.validator.messages, {
-	      required: "This field is required.",
+	required: "This field is required.",
         remote: "Please fix this field.",
         email: "Please enter a valid email address.",
         url: "Please enter a valid URL.",
