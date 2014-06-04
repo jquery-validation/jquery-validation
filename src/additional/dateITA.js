cb0a8529f41c8437b9125a5190f0365ae40d618d
lboullo0 <lucasboullosa@yahoo.com.ar>
2014-06-04 19:48:26 -0300

    Update dateITA.js
    Replace == by === to correct build.

--- src/additional/dateITA.js
+++ src/additional/dateITA.js
@@ -27,7 +27,7 @@ $.validator.addMethod("dateITA", function(value, element) {
 		mm = parseInt(adata[1],10);
 		aaaa = parseInt(adata[2],10);
 		xdata = new Date(aaaa, mm - 1, gg, 12, 0, 0, 0);
-		if ( ( xdata.getUTCFullYear() == aaaa ) && ( xdata.getUTCMonth () == mm - 1 ) && ( xdata.getUTCDate() == gg ) ) {
+		if ( ( xdata.getUTCFullYear() === aaaa ) && ( xdata.getUTCMonth () === mm - 1 ) && ( xdata.getUTCDate() === gg ) ) {
 			check = true;
 		} else {
 			check = false;
