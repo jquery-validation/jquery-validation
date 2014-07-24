bd94154964c368fd3d6f9a7b1aaacf50ab29482f
Pawel <shorstki@gmail.com>
2014-07-24 13:19:04 +0200

    Fix core.js for IE 8 support
    var validator was undefined on IE 8

--- src/core.js
+++ src/core.js
@@ -990,7 +990,7 @@ $.extend( $.validator, {
 
 	staticRules: function( element ) {
 		var rules = {},
-			validator = $.data( element.form, "validator" );
+			validator = $.extend(true, {settings: {rules: {}}}, $.data( element.form, "validator" ));
 
 		if ( validator.settings.rules ) {
 			rules = $.validator.normalizeRule( validator.settings.rules[ element.name ] ) || {};
