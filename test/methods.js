jquery-validation/jquery-validation

commit 37992c1c9e2e0be8b315ccccc2acb74863439d3e

Pattern method: Remove delimiters, since HTML5 implementations don't include those either.

src/additional/pattern.js
@@ -16,7 +16,7 @@ jQuery.validator.addMethod("pattern", function(value, element, param) {
 		return true;
 	}
 	if (typeof param === 'string') {
-		param = new RegExp('^(?:' + param + ')$');
+		param = new RegExp(param);
 	}
 	return param.test(value);
 }, "Invalid format.");

commit e61e8452944c5fea06c479701476c48642b9d8bb

Fix html5 input pattern validation
It seems this commit: https://github.com/jzaefferer/jquery-validation/commit/37992c1c9e2e0be8b315ccccc2acb74863439d3e broke html5 input pattern validation.

The pattern should match the entire value of the input, not just a subset.

Added tests using the example from http://www.w3.org/TR/html5/forms.html#the-pattern-attribute
First 2 were working already, 3rd is to prevent regression. Also added 2 common patterns, alphanumeric and a date (from http://html5pattern.com/)

src/additional/pattern.js
@@ -16,7 +16,7 @@ $.validator.addMethod("pattern", function(value, element, param) {
 		return true;
 	}
 	if (typeof param === "string") {
-		param = new RegExp(param);
+		param = new RegExp("^(?:" + param + ")$");
 	}
 	return param.test(value);
 }, "Invalid format.");

test/methods.js
@@ -918,6 +918,13 @@ test("pattern", function() {
 	ok( method( "AR1004", "AR\\d{4}" ), "Correct format for the given RegExp" );
 	ok( method( "AR1004", /^AR\d{4}$/ ), "Correct format for the given RegExp" );
 	ok(!method( "BR1004", /^AR\d{4}$/ ), "Invalid format for the given RegExp" );
+	ok( method( "1ABC", "[0-9][A-Z]{3}" ), "Correct format for the given RegExp" );
+	ok(!method( "ABC", "[0-9][A-Z]{3}" ), "Invalid format for the given RegExp" );
+	ok(!method( "1ABC DEF", "[0-9][A-Z]{3}" ), "Invalid format for the given RegExp" );
+	ok( method( "1ABCdef", "[a-zA-Z0-9]+" ), "Correct format for the given RegExp" );
+	ok(!method( "1ABC def", "[a-zA-Z0-9]+" ), "Invalid format for the given RegExp" );
+	ok( method( "2014-10-02", "[0-9]{4}-(0[1-9]|1[012])-(0[1-9]|1[0-9]|2[0-9]|3[01])" ), "Correct format for the given RegExp" );
+	ok(!method( "02-10-2014", "[0-9]{4}-(0[1-9]|1[012])-(0[1-9]|1[0-9]|2[0-9]|3[01])" ), "Invalid format for the given RegExp" );
 });
 
 function testCardTypeByNumber(number, cardname, expected) {