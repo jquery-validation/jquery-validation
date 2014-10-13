05d4f4c5158351db86218e849db290bce2a98f23
fernandopasse <fernandopasse@hotmail.com>
2014-10-12 22:27:53 -0300

    Otimizar: Melhorar expressão regular de validação

--- src/additional/postalcodeBR.js
+++ src/additional/postalcodeBR.js
@@ -6,11 +6,6 @@
 * 99.999-999
 * 99999999
 */
-$.validator.addMethod("postalcodeBR", function( cep_value ) {
-	if ( /^\d{2}.\d{3}-\d{3}?$/.test( cep_value ) ){
-		return true;
-	}else if ( /^\d{5}-?\d{3}?$/.test( cep_value ) ){
-			return true;
-	}
-	return false;
+$.validator.addMethod("postalcodeBR", function(cep_value, element) {
+	return this.optional(element) || /^\d{2}.\d{3}-\d{3}?$|^\d{5}-?\d{3}?$/.test( cep_value );
 }, "Informe um CEP válido.");
