495740ddce1cbc94e5bcfcf434256015a91d3333
fernandopasse <fernandopasse@hotmail.com>
2015-02-24 23:16:45 -0300

    teBR.js: Corrigir

--- src/additional/teBR.js
+++ src/additional/teBR.js
@@ -9,31 +9,31 @@
  * 99999999999
  * 999999999/99
  */
-$.validator.addMethod("teBR", function(numero_titulo) {
-	var pesos = "23456789", resto_dv1 = 0, resto_dv2 = 0, i;
-	if (!(/^\d{12}$|^\d{4} \d{4} \d{4}$/.test(numero_titulo))) {
-		numero_titulo = "0" + numero_titulo;
-		if (!(/^\d{12}$|^\d{10}\/\d{2}$/.test(numero_titulo))) {
+$.validator.addMethod("teBR", function(number_title) {
+	var weights = "23456789", rest_dv1 = 0, rest_dv2 = 0, i;
+	if (!(/^\d{12}$|^\d{4} \d{4} \d{4}$/.test(number_title))) {
+		number_title = "0" + number_title;
+		if (!(/^\d{12}$|^\d{10}\/\d{2}$/.test(number_title))) {
 			return false;
 		}
 	}
-	numero_titulo = numero_titulo.replace(/[^\d]+/g, "");
+	number_title = number_title.replace(/[^\d]+/g, "");
 	for (i = 0; i <= 8; i++) {
-		resto_dv1 += (numero_titulo.charAt(i)) * (pesos.charAt(i));
+		rest_dv1 += (number_title.charAt(i)) * (weights.charAt(i));
 	}
 	for (i = 8; i <= 10; i++) {
-		resto_dv2 += (numero_titulo.charAt(i)) * (pesos.charAt(i - 3));
+		rest_dv2 += (number_title.charAt(i)) * (weights.charAt(i - 3));
 	}
-	resto_dv1 %= 11;
-	resto_dv2 %= 11;
-	if (resto_dv1 === 10) {
-		resto_dv1 = 0;
+	rest_dv1 %= 11;
+	rest_dv2 %= 11;
+	if (rest_dv1 === 10) {
+		rest_dv1 = 0;
 	}
-	if (resto_dv2 === 10) {
-		resto_dv2 = 0;
+	if (rest_dv2 === 10) {
+		rest_dv2 = 0;
 	}
-	if ((String(resto_dv1) === numero_titulo.charAt(10)) && (String(resto_dv2) === numero_titulo.charAt(11))) {
+	if ((String(rest_dv1) === number_title.charAt(10)) && (String(rest_dv2) === number_title.charAt(11))) {
 		return true;
 	}
 	return false;
-}, "Please specify a valid &quot;T&iacute;tulo&quot; number.");
+}, "Please specify a valid number.");
