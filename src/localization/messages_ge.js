91acfe6629061962e2a16a11659b01c7d84ea94a
Merab Tato Kutalia <tatocaster@users.noreply.github.com>
2015-04-09 18:43:19 +0400

    update messages_ge.js

--- src/localization/messages_ge.js
+++ src/localization/messages_ge.js
@@ -1,11 +1,3 @@
-(function( factory ) {
-	if ( typeof define === "function" && define.amd ) {
-		define( ["jquery", "../jquery.validate"], factory );
-	} else {
-		factory( jQuery );
-	}
-}(function( $ ) {
-
 /**
  * @author  @tatocaster <kutaliatato@gmail.com>
  * Translated default messages for the jQuery validation plugin.
@@ -29,5 +21,3 @@ $.extend($.validator.messages, {
 	max: $.validator.format( "გთხოვთ შეიყვანოთ მნიშვნელობა ნაკლები ან ტოლი {0} -ს." ),
 	min: $.validator.format( "გთხოვთ შეიყვანოთ მნიშვნელობა მეტი ან ტოლი {0} -ს." )
 });
-
-}));
