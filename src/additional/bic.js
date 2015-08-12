aca55bd30f01a350cfe7e6696caea1fcb3eacf0e
netzkind <julian@makowskis.de>
2015-08-12 09:01:33 +0200

    case-insensitive check for BIC
    I found no appearant reason why a BIC has to be all-upper, so allowing customers to enter it lowercase or mixed should be ok.
    Furthermore, the IBAN-validation also is case-insensitive.

--- src/additional/bic.js
+++ src/additional/bic.js
@@ -12,5 +12,5 @@
  * - Last 3 characters - branch code, optional (shall not start with 'X' except in case of 'XXX' for primary office) (letters and digits)
  */
 $.validator.addMethod("bic", function(value, element) {
-    return this.optional( element ) || /^([A-Z]{6}[A-Z2-9][A-NP-Z1-2])(X{3}|[A-WY-Z0-9][A-Z0-9]{2})?$/.test( value );
+    return this.optional( element ) || /^([A-Z]{6}[A-Z2-9][A-NP-Z1-2])(X{3}|[A-WY-Z0-9][A-Z0-9]{2})?$/.test( value.toUpperCase() );
 }, "Please specify a valid BIC code");
