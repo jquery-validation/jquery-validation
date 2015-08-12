9b8cbbfc635ec31af67d6fe2ada6e7fc7ca7d117
netzkind <julian@makowskis.de>
2015-08-12 13:00:32 +0200

    case-insensitive check for BIC

--- src/additional/bic.js
+++ src/additional/bic.js
@@ -2,6 +2,8 @@
  * BIC is the business identifier code (ISO 9362). This BIC check is not a guarantee for authenticity.
  *
  * BIC pattern: BBBBCCLLbbb (8 or 11 characters long; bbb is optional)
+ * 
+ * Validation is case-insensitive. Please make sure to normalize input yourself.
  *
  * BIC definition in detail:
  * - First 4 characters - bank code (only letters)
