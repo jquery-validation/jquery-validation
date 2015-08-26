ff45b444b2064bbb2e6dec6124e1dfa9ff97ccbd
Ed Akagawa <edakagawa@gmail.com>
2015-08-26 18:03:41 -0300

    Update phoneBR.js

--- src/additional/phoneBR.js
+++ src/additional/phoneBR.js
@@ -2,4 +2,4 @@ $.validator.addMethod("phoneBR", function(phone_number, element) {
 	phone_number = phone_number.replace(/\s+/g, "");
 	return this.optional(element) || phone_number.length > 7 &&
 		phone_number.match(/^(\(?[0-9]{2,3}\)?)? ?([0-9]{4,5})-?([0-9]{4})$/gm);
-}, "Digite um numero de telefone");
+}, "Please specify a valid phone number");
