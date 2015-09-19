4d3059e09b921091b6d287ed4be78e9fb728dac7
Brahim Arkni <brahim.arkni@gmail.com>
2015-09-19 23:46:17 +0100

    Additional: Fixed spacing issues.

--- src/additional/postalcodeBR.js
+++ src/additional/postalcodeBR.js
@@ -6,6 +6,6 @@
 * 99.999-999
 * 99999999
 */
-$.validator.addMethod("postalcodeBR", function(cep_value, element) {
-	return this.optional(element) || /^\d{2}.\d{3}-\d{3}?$|^\d{5}-?\d{3}?$/.test( cep_value );
-}, "Informe um CEP válido.");
+$.validator.addMethod( "postalcodeBR", function( cep_value, element ) {
+	return this.optional( element ) || /^\d{2}.\d{3}-\d{3}?$|^\d{5}-?\d{3}?$/.test( cep_value );
+}, "Informe um CEP válido." );
