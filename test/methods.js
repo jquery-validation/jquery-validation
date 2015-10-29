f5b63176c8e86a14163f5f48b68a7e14e1f786b2
Alok Saldanha <alok.saldanha@novartis.com>
2015-10-29 15:23:47 -0400

    Test: updated test for "pending" remote status
    Removed unnecessary submitHandler method
    Whitespace fixes
    Replaced ajaxStop hack with complete method

--- test/methods.js
+++ test/methods.js
@@ -396,30 +396,27 @@ asyncTest( "remote", function() {
 	strictEqual( v.element( e ), true, "still invalid, because remote validation must block until it returns; dependency-mismatch considered as valid though" );
 } );
 
-asyncTest("remote, pending class added to element while call outstanding", function() {
-	expect(3);
-	var e = $("#username"),
-		v = $("#userForm").validate({
+asyncTest( "remote, pending class added to element while call outstanding", function() {
+	expect( 3 );
+	var e = $( "#username" ),
+		v = $( "#userForm" ).validate( {
 			rules: {
 				username: {
-					remote: "users.php"
+					remote: {
+						url: "users.php",
+						complete: function() {
+							strictEqual( e.hasClass( "pending" ), false, "not pending since ajax call complete" );
+							start();
+						}
+					}
 				}
-			},
-			submitHandler: function() {
-				ok( false, "submitHandler may never be called when validating only elements");
 			}
-		});
-
-	$(document).ajaxStop(function() {
-		$(document).unbind("ajaxStop");
-	  strictEqual( e.hasClass("pending"), false, "not pending since ajax call complete");
-		start();
-	});
-	strictEqual( e.hasClass("pending"), false, "not pending since no data entered");
-	e.val("Peter");
+		} );
+	strictEqual( e.hasClass("pending"), false, "not pending since no data entered" );
+	e.val( "Peter" );
 	// this fires off the validation:
-	v.element(e);
-	strictEqual( e.hasClass("pending"), true, "pending while validation outstanding");
+	v.element( e );
+	strictEqual( e.hasClass( "pending" ), true, "pending while validation outstanding" );
 });
 
 asyncTest( "remote, customized ajax options", function() {
