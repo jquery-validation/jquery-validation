89ccdf79a8d7373476ef09ae6584833de02359e0
Markus Staab <maggus.staab@googlemail.com>
2015-11-12 16:50:52 +0100

    Core: fixed typo in internal findLastActive method

--- src/core.js
+++ src/core.js
@@ -562,7 +562,7 @@ $.extend( $.validator, {
 			var lastActive = this.lastActive;
 			return lastActive && $.grep( this.errorList, function( n ) {
 				return n.element.name === lastActive.name;
-			} ).length === 1 && lastActive;
+			} ).length === 1;
 		},
 
 		elements: function() {
