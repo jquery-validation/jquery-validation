50adefbb991edf650a086a9c5cc19bc2e1a133ef
NiranjanaK <niranjana.kamaraj@gmail.com>
2016-01-12 19:14:21 +0530

    Additional: Modified Name

--- src/additional/passwordstrength.js
+++ src/additional/passwordstrength.js
@@ -1,3 +1,3 @@
-$.validator.addMethod( "pwcheck", function( value ) {
+$.validator.addMethod( "password-strength", function( value ) {
    return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,}/.test( value );
 }, "Password should be 8 characters length and contain at least one uppercase, lowercase, digit and special character." );
