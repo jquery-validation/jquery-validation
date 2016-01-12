78d2cfcdb6de81c3ce81e9c64b4d82bf543b643b
NiranjanaK <niranjana.kamaraj@gmail.com>
2016-01-12 18:16:57 +0530

    Additional: corrected spaces

--- src/additional/passwordstrength.js
+++ src/additional/passwordstrength.js
@@ -1,3 +1,3 @@
-$.validator.addMethod("pwcheck", function(value) {
-   return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,}/.test(value) // consists of only these
-    },'Password should be 8 characters length and contain at least one uppercase,lowercase,digit and special character');
+$.validator.addMethod( "pwcheck", function( value ) {
+   return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,}/.test( value )
+    }, 'Password should be 8 characters length and contain at least one uppercase,lowercase,digit and special character' );
