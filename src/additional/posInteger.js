73d69620afbd2b5806de3b3894ad16f177ea7463
Chris A. Vollink <chris@vollink.com>
2016-03-11 09:39:43 -0500

    fixed style errors

--- src/additional/posInteger.js
+++ src/additional/posInteger.js
@@ -1,5 +1,3 @@
-// allow only positive integers
-
-jQuery.validator.addMethod("posInteger", function(value, element) {
+jQuery.validator.addMethod( "posInteger", function( value, element ) {
 	return this.optional( element ) || /\A[0-9]+\z/.test( value ) || /^[0-9]+$/.test( value );
-}, 'Please enter a positive non-decimal number.');
+}, "Please enter a positive non-decimal number." );
