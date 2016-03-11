a65f65f93f3ce7882100122a183e1c5befae8255
Chris A. Vollink <chris@vollink.com>
2016-03-11 09:31:13 -0500

    add validate method for positive integers

--- src/additional/posInteger.js
+++ src/additional/posInteger.js
@@ -1,3 +1,5 @@
+// allow only positive integers
+
 jQuery.validator.addMethod("posInteger", function(value, element) {
 	return this.optional( element ) || /\A[0-9]+\z/.test( value ) || /^[0-9]+$/.test( value );
 }, 'Please enter a positive non-decimal number.');
