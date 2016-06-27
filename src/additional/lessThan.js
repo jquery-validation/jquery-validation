acb0cbe83e804aad17b31f36dcfff99ef5162a8d
Dennis Hernández <dennishernandezvargas@gmail.com>
2016-06-27 13:30:44 -0600

    Added greaterThan and lessThan validations

---
+++ src/additional/lessThan.js
@@ -0,0 +1,4 @@
+$.validator.addMethod("lessThan", function (value, element, params) {
+    var currentValue = isNaN(value) ? undefined : parseFloat(value);
+    return this.optional(element) || currentValue < params[0];
+}, $.validator.format("The value must be less than {0}."));
