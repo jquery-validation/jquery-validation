45997eceed90fb3fa6a6ccfee846fab617d44367
Kevin Morris <kevinqmorris@gmail.com>
2016-07-23 11:52:10 -0400

    Notates jQ 1.7 backwards compatibility line

--- src/core.js
+++ src/core.js
@@ -186,7 +186,7 @@ $.extend( $.fn, {
 } );
 
 // Custom selectors
-$.extend( $.expr.pseudos || $.expr[ ":" ], {
+$.extend( $.expr.pseudos || $.expr[ ":" ], {		// '|| $.expr[ ":" ]' here enables backwards compatibility to jQuery 1.7. Can be removed when dropping jQ 1.7.x support
 
 	// http://jqueryvalidation.org/blank-selector/
 	blank: function( a ) {
