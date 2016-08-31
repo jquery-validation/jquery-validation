61218d3042a056ac0eb4b465e9d78b10d4ac8c21
Dominik Krzywiecki <dominik@krzywiecki.com>
2016-08-31 17:04:47 +0200

    Localization: Add message for Polish tax id number (NIP) method

--- src/localization/messages_pl.js
+++ src/localization/messages_pl.js
@@ -14,6 +14,7 @@ $.extend( $.validator.messages, {
 	creditcard: "Proszę o podanie prawidłowej karty kredytowej.",
 	equalTo: "Proszę o podanie tej samej wartości ponownie.",
 	extension: "Proszę o podanie wartości z prawidłowym rozszerzeniem.",
+	nipPL: "Proszę o podanie prawidłowego numeru NIP.",
 	maxlength: $.validator.format( "Proszę o podanie nie więcej niż {0} znaków." ),
 	minlength: $.validator.format( "Proszę o podanie przynajmniej {0} znaków." ),
 	rangelength: $.validator.format( "Proszę o podanie wartości o długości od {0} do {1} znaków." ),
