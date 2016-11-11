5b6632bb59ef61d7478868ea3c30b60fa6e06e25
Jonathan <vanillajonathan@users.noreply.github.com>
2016-11-11 17:02:40 +0100

    Update creditcard.js

--- src/additional/creditcard.js
+++ src/additional/creditcard.js
@@ -1,5 +1,5 @@
-// http://jqueryvalidation.org/creditcard-method/
-// based on http://en.wikipedia.org/wiki/Luhn_algorithm
+// https://jqueryvalidation.org/creditcard-method/
+// based on https://en.wikipedia.org/wiki/Luhn_algorithm
 $.validator.addMethod( "creditcard", function( value, element ) {
 	if ( this.optional( element ) ) {
 		return "dependency-mismatch";
@@ -18,7 +18,7 @@ $.validator.addMethod( "creditcard", function( value, element ) {
 	value = value.replace( /\D/g, "" );
 
 	// Basing min and max length on
-	// http://developer.ean.com/general_info/Valid_Credit_Card_Types
+	// https://developer.ean.com/general_info/Valid_Credit_Card_Types
 	if ( value.length < 13 || value.length > 19 ) {
 		return false;
 	}
