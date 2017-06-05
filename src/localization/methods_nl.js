3ef8d7818221785a48b26792c9df2128d2ef79bf
Bas van Marwijk <basvmar@hotmail.com>
2017-06-05 17:42:02 +0200

    methods_nl.js: format code according to coding style

--- src/localization/methods_nl.js
+++ src/localization/methods_nl.js
@@ -6,7 +6,7 @@ $.extend( $.validator.methods, {
 	date: function( value, element ) {
 		return this.optional( element ) || /^\d\d?[\.\/\-]\d\d?[\.\/\-]\d\d\d?\d?$/.test( value );
 	},
-	number: function (value, element) {
-        return this.optional(element) || /^-?(?:\d+|\d{1,3}(?:\.\d{3})+)(?:,\d+)?$/.test(value);
+	number: function( value, element ) {
+        return this.optional( element ) || /^-?(?:\d+|\d{1,3}(?:\.\d{3})+)(?:,\d+)?$/.test( value );
     }
 } );
