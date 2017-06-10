56ccc29bae1cc37575a2c916f0b525bc9aaf4692
Brahim Arkni <brahim.arkni@gmail.com>
2017-06-10 23:30:27 +0000

    Fix indentation

--- src/localization/methods_nl.js
+++ src/localization/methods_nl.js
@@ -7,6 +7,6 @@ $.extend( $.validator.methods, {
 		return this.optional( element ) || /^\d\d?[\.\/\-]\d\d?[\.\/\-]\d\d\d?\d?$/.test( value );
 	},
 	number: function( value, element ) {
-        return this.optional( element ) || /^-?(?:\d+|\d{1,3}(?:\.\d{3})+)(?:,\d+)?$/.test( value );
-    }
+		return this.optional( element ) || /^-?(?:\d+|\d{1,3}(?:\.\d{3})+)(?:,\d+)?$/.test( value );
+	}
 } );
