eb464fb124abdb39c1f162cbd67e96f77b4e1c66
cleber s leite <clebersleite@gmail.com>
2017-08-31 08:09:23 -0300

    Update dateBR.js
    Better format like other, using regex

--- src/additional/dateBR.js
+++ src/additional/dateBR.js
@@ -1,14 +1,3 @@
-$.validator.addMethod("dateBR", function(value, element) {            
-                    if(value.length!=10) return false;
-                    var data        = value;
-                    var dia         = data.substr(0,2);
-                    var barra1      = data.substr(2,1);
-                    var mes         = data.substr(3,2);         
-                    var barra2      = data.substr(5,1);
-                    var ano         = data.substr(6,4);         
-                    if(data.length!=10||barra1!="/"||barra2!="/"||isNaN(dia)||isNaN(mes)||isNaN(ano)||dia>31||mes>12) return false; 
-                    if((mes==4||mes==6||mes==9||mes==11) && dia==31) return false;
-                    if(mes==2 && (dia>29||(dia==29 && ano%4!=0))) return false;
-                    if(ano < 1900)  return false;                    
-                    return true;        
-}, $.validator.messages.date);
+$.validator.addMethod( "dateBRt", function( value, element ) {
+	                    return this.optional( element ) || /^(((((0[1-9])|(1\d)|(2[0-8]))\/((0[1-9])|(1[0-2])))|((31\/((0[13578])|(1[02])))|((29|30)\/((0[1,3-9])|(1[0-2])))))\/((20[0-9][0-9])|(19[0-9][0-9])))|((29\/02\/(19|20)(([02468][048])|([13579][26]))))$/.test( value );
+}, $.validator.messages.date );
