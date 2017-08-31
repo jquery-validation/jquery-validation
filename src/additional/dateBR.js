85c9f61c04869b65a9d58d4ff704a6ab0cc5e42e
cleber s leite <clebersleite@gmail.com>
2017-08-31 07:58:42 -0300

    Update dateBR.js
    Changed translate method, changed JQuery for $

--- src/additional/dateBR.js
+++ src/additional/dateBR.js
@@ -1,4 +1,4 @@
-jQuery.validator.addMethod("dateBR", function(value, element) {            
+$.validator.addMethod("dateBR", function(value, element) {            
                     if(value.length!=10) return false;
                     var data        = value;
                     var dia         = data.substr(0,2);
@@ -9,7 +9,6 @@ jQuery.validator.addMethod("dateBR", function(value, element) {
                     if(data.length!=10||barra1!="/"||barra2!="/"||isNaN(dia)||isNaN(mes)||isNaN(ano)||dia>31||mes>12) return false; 
                     if((mes==4||mes==6||mes==9||mes==11) && dia==31) return false;
                     if(mes==2 && (dia>29||(dia==29 && ano%4!=0))) return false;
-                    if(ano < 1900)  return false;
-                    
+                    if(ano < 1900)  return false;                    
                     return true;        
-}, "Informe uma data válida");
+}, $.validator.messages.date);
