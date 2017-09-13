9e0f1016efa50575b5bfc2a8cbbef37b75a52072
Hawkon <HawkonDK@users.noreply.github.com>
2017-09-13 18:15:33 +0200

    Update messages_da.js

--- src/localization/messages_da.js
+++ src/localization/messages_da.js
@@ -15,7 +15,7 @@ $.extend( $.validator.messages, {
 	time: "Angiv en gyldig tid mellem kl. 00:00 og 23:59.",
 	ipv4: "Angiv venligst en gyldig IPv4-adresse.",
 	ipv6: "Angiv venligst en gyldig IPv6-adresse.",
-	require_from_group:  $.validator.format("Angiv mindst {0} af disse felter."),
+	require_from_group:  $.validator.format( "Angiv mindst {0} af disse felter." ),
 	extension: "Indtast venligst en værdi med en gyldig endelse",
 	pattern: "Ugyldigt format",
 	lettersonly: "Angiv venligst kun bogstaver.",
@@ -26,9 +26,9 @@ $.extend( $.validator.messages, {
 	range: $.validator.format( "Angiv en værdi mellem {0} og {1}." ),
 	max: $.validator.format( "Angiv en værdi der højst er {0}." ),
 	min: $.validator.format( "Angiv en værdi der mindst er {0}." ),
-	minWords: $.validator.format("Indtast venligst mindst {0} ord"),
-	maxWords:  $.validator.format("Indtast venligst højst {0} ord"),
-	step: $.validator.format( "Angiv en værdi gange {0}."),
+	minWords: $.validator.format( "Indtast venligst mindst {0} ord" ),
+	maxWords:  $.validator.format( "Indtast venligst højst {0} ord" ),
+	step: $.validator.format( "Angiv en værdi gange {0}." ),
 	notEqualTo: "Angiv en anden værdi, værdierne må ikke være det samme.",
 	integer: "Angiv et ikke-decimaltal, der er positivt eller negativt."
 } );
