d70eb7a8f95c6aac40a3aa3dcc96b15511a80825
Brahim Arkni <brahim.arkni@gmail.com>
2017-10-08 21:22:30 +0100

    Additional: remove duplicate check

--- src/additional/maxfiles.js
+++ src/additional/maxfiles.js
@@ -5,7 +5,7 @@ $.validator.addMethod( "maxfiles", function( value, element, param ) {
 	}
 
 	if ( $( element ).attr( "type" ) === "file" ) {
-		if ( element.files && element.files.length && element.files.length > param ) {
+		if ( element.files && element.files.length > param ) {
 			return false;
 		}
 	}
