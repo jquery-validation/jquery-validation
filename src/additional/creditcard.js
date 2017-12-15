d4541614ec2748efbebb5c4a74fb15fe543ac8ab
William Desportes <williamdes@wdes.fr>
2017-12-15 10:19:10 +0100

    Additional: Updated link to EAN docs (#2120)

--- src/additional/creditcard.js
+++ src/additional/creditcard.js
@@ -18,7 +18,7 @@ $.validator.addMethod( "creditcard", function( value, element ) {
 	value = value.replace( /\D/g, "" );
 
 	// Basing min and max length on
-	// https://developer.ean.com/general_info/Valid_Credit_Card_Types
+	// https://dev.ean.com/general-info/valid-card-types/
 	if ( value.length < 13 || value.length > 19 ) {
 		return false;
 	}
