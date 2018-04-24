64c3401d09a272946b7ee89861f90d1b7b79655b
Benjamin Høegh <22152591+BenjaminHoegh@users.noreply.github.com>
2018-04-24 11:36:21 +0200

    Fix for Invalid line break

--- src/additional/phoneDK.js
+++ src/additional/phoneDK.js
@@ -1,25 +1,25 @@
 /**
- * Matches DK phone number format
- *
- * allows '-' or ' ' as a separator and allows parens around area code
- * some people may want to put a '+45' in front of their number
- *
- * +45 21-77-21-77
- * +45 21 77 21 77
- * +45 21772177
- * +4521772177
- * 21 77 21 77
- * 21-77-21-77
- * 2177 2177
- * 2177-2177
- * 21772177
- *
- * but not
- * +1 3243121212
- * +45 112
- * +452143284233
- * 2143284233
- */
+* Matches DK phone number format
+*
+* allows '-' or ' ' as a separator and allows parens around area code
+* some people may want to put a '+45' in front of their number
+*
+* +45 21-77-21-77
+* +45 21 77 21 77
+* +45 21772177
+* +4521772177
+* 21 77 21 77
+* 21-77-21-77
+* 2177 2177
+* 2177-2177
+* 21772177
+*
+* but not
+* +1 3243121212
+* +45 112
+* +452143284233
+* 2143284233
+*/
 $.validator.addMethod( "phoneDK", function( phone_number, element ) {
 	phone_number = phone_number.replace( /\s+/g, "" );
 	return this.optional( element ) || phone_number.length > 9 &&
