ffe9a50e6cdaef3ab3d6540626c490acd86726a1
Jorge R <eu.jorgerafael@gmail.com>
2018-09-13 18:40:19 -0300

    Fix: bug with summernote code

--- src/core.js
+++ src/core.js
@@ -268,6 +268,7 @@ $.extend( $.validator, {
 		ignore: ":hidden",
 		ignoreTitle: false,
 		onfocusin: function( element ) {
+			element = this.isSummernote(element); // To Fix Bug with Summernote Plugin
 			this.lastActive = element;
 
 			// Hide error label and remove error class on focus if enabled
@@ -279,12 +280,13 @@ $.extend( $.validator, {
 			}
 		},
 		onfocusout: function( element ) {
+			element = this.isSummernote(element); // To Fix Bug with Summernote Plugin
 			if ( !this.checkable( element ) && ( element.name in this.submitted || !this.optional( element ) ) ) {
 				this.element( element );
 			}
 		},
 		onkeyup: function( element, event ) {
-
+			element = this.isSummernote(element); // To Fix Bug with Summernote Plugin
 			// Avoid revalidate the field when pressing one of the following keys
 			// Shift       => 16
 			// Ctrl        => 17
@@ -311,7 +313,7 @@ $.extend( $.validator, {
 			}
 		},
 		onclick: function( element ) {
-
+			element = this.isSummernote(element); // To Fix Bug with Summernote Plugin
 			// Click on selects, radiobuttons and checkboxes
 			if ( element.name in this.submitted ) {
 				this.element( element );
@@ -322,6 +324,7 @@ $.extend( $.validator, {
 			}
 		},
 		highlight: function( element, errorClass, validClass ) {
+			element = this.isSummernote(element); // To Fix Bug with Summernote Plugin
 			if ( element.type === "radio" ) {
 				this.findByName( element.name ).addClass( errorClass ).removeClass( validClass );
 			} else {
@@ -329,6 +332,7 @@ $.extend( $.validator, {
 			}
 		},
 		unhighlight: function( element, errorClass, validClass ) {
+			element = this.isSummernote(element); // To Fix Bug with Summernote Plugin
 			if ( element.type === "radio" ) {
 				this.findByName( element.name ).removeClass( errorClass ).addClass( validClass );
 			} else {
@@ -1037,7 +1041,12 @@ $.extend( $.validator, {
 			// Always apply ignore filter
 			return $( element ).not( this.settings.ignore )[ 0 ];
 		},
-
+		isSummernote: function(element) {
+			if ($(element).hasClass('note-editable')) {
+				return $(element).parent().parent().parent().find('textarea')
+			}
+			return element;
+		},
 		checkable: function( element ) {
 			return ( /radio|checkbox/i ).test( element.type );
 		},
