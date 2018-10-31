f92789d98387e03dad924171fa16dff27356e298
João Issamu Francisco <issamu@gmail.com>
2018-10-31 18:15:50 -0300

    Update and rename cnhBr.js to cnhBR.js

--- test/additional/cnhBR.js
+++ test/additional/cnhBR.js
@@ -1,5 +1,5 @@
-QUnit.test( "cnhbr", function( assert ) {
-	var method = methodTest( "cnhbr" );
+QUnit.test( "cnhBR", function( assert ) {
+	var method = methodTest( "cnhBR" );
 	assert.ok( method( "00000000119" ), "Valid driver's license number" );
 	assert.ok( !method( "11111111111" ), "Invalid driver's license number" );
 	assert.ok( !method( "asdf" ), "Invalid driver's license number" );
