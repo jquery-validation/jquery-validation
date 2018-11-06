3242f0f3e7ee1db1de6b9c8e295072a9aa5d8d63
jehadja <jehad.jaghoub2@gmail.com>
2018-11-06 22:53:31 +0300

    Additional: Add validation rule for mobile number of Russia Federation (#2207)

---
+++ src/additional/mobileRU.js
@@ -0,0 +1,4 @@
+$.validator.addMethod( "mobileRU", function( phone_number, element ) {
+	var ruPhone_number = phone_number.replace( /\(|\)|\s+|-/g, "" );
+	return this.optional( element ) || ruPhone_number.length > 9 && /^((\+7|7|8)+([0-9]){10})$/.test( ruPhone_number );
+}, "Please specify a valid mobile number" );
