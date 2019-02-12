a63e558daa2e9f3b768d424992e3a5026335c650
garima <garima@commons-MacBook-Air-2.local>
2019-02-12 11:32:02 +0530

    Core:Removed console log

--- src/core.js
+++ src/core.js
@@ -625,8 +625,6 @@ $.extend( $.validator, {
 				rulesCache = {};
 
 			// Select all valid inputs inside the form (no submit or reset buttons)
-			console.error( $.merge( $( this.currentForm ).find( "input, select, textarea, [contenteditable]" ),
-            $( this.currentForm.elements ).filter( "input, select, textarea, [contenteditable]" ) ) );
 			return $.merge( $( this.currentForm ).find( "input, select, textarea, [contenteditable]" ),
             $( this.currentForm.elements ).filter( "input, select, textarea, [contenteditable]" ) )
 			.not( ":submit, :reset, :image, :disabled" )
