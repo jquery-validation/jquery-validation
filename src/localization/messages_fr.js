b8d6646ec67c73372dddfbc9aadff45571a96136
Rob Johnston <RobJohnston@users.noreply.github.com>
2020-11-28 02:23:53 -0500

    Localization: Add "pattern" translation for French (#2363)

--- src/localization/messages_fr.js
+++ src/localization/messages_fr.js
@@ -47,5 +47,6 @@ $.extend( $.validator.messages, {
 	nifES: "Veuillez fournir un numéro NIF valide.",
 	nieES: "Veuillez fournir un numéro NIE valide.",
 	cifES: "Veuillez fournir un numéro CIF valide.",
-	postalCodeCA: "Veuillez fournir un code postal valide."
+	postalCodeCA: "Veuillez fournir un code postal valide.",
+	pattern: "Format non valide."
 } );
