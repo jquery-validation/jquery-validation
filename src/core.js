7c32be5943a91f515896b35740d2bcbdadd4e7b1
amigian74 <steven.woelk@papendorf.info>
2021-06-04 10:20:12 +0200

    Convert exponential step values

--- src/core.js
+++ src/core.js
@@ -1501,6 +1501,24 @@ $.extend( $.validator, {
 					// Number of digits right of decimal point.
 					return match[ 1 ] ? match[ 1 ].length : 0;
 				},
+				toFixed = function(x) {
+					let e;
+					if (Math.abs(x) < 1.0) {
+						e = parseInt(x.toString().split('e-')[1]);
+						if (e) {
+							x *= Math.pow(10,e-1);
+							x = '0.' + (new Array(e)).join('0') + x.toString().substring(2);
+						}
+					} else {
+						e = parseInt(x.toString().split('+')[1]);
+						if (e > 20) {
+							e -= 20;
+							x /= Math.pow(10,e);
+							x += (new Array(e+1)).join('0');
+						}
+					}
+					return x;
+				},
 				toInt = function( num ) {
 					return Math.round( num * Math.pow( 10, decimals ) );
 				},
@@ -1513,7 +1531,7 @@ $.extend( $.validator, {
 				throw new Error( errorMessage );
 			}
 
-			decimals = decimalPlaces( param );
+			decimals = decimalPlaces( toFixed(param) );
 
 			// Value can't have too many decimals
 			if ( decimalPlaces( value ) > decimals || toInt( value ) % toInt( param ) !== 0 ) {
