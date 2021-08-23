1e47b0842acc95001913353ab04b887e0a321883
Iván Oliva Reglero <ioliva@users.noreply.github.com>
2021-08-23 09:54:31 +0200

    Better matching code style
    Better respecting quotating marks and spacing conventions.

--- src/additional/pattern.js
+++ src/additional/pattern.js
@@ -18,15 +18,13 @@ $.validator.addMethod( "pattern", function( value, element, param ) {
 	if ( this.optional( element ) ) {
 		return true;
 	}
-
 	var ignoreCase = false;
 	if ( Array.isArray( param ) ) {
-		ignoreCase = (typeof param[1] === 'boolean' && param[1]);
+		ignoreCase = (typeof param[1] === "boolean" && param[1]);
 		param = param[0];
 	}
-
-	if (typeof param === 'string') {
-		param = new RegExp('^(?:' + param + ')$', ignoreCase ? 'i' : '');
+	if ( typeof param === "string" ) {
+		param = new RegExp( "^(?:" + param + ")$", ignoreCase ? "i" : "" );
 	}
 	return param.test( value );
 }, "Invalid format." );
