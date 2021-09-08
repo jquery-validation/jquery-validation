75bdef94b3805c8726b9a11e8a8a6fcc733279a8
Deler T. Abdolqader <62033770+Deler-T-Abdolqader@users.noreply.github.com>
2021-09-08 13:03:36 +0300

    add Ku (Central Kurdish) to localizaion

---
+++ src/localization/messages_ku.js
@@ -0,0 +1,27 @@
+/*
+ * Translated default messages for the jQuery validation plugin.
+ * Locale: KU (Centeral Kurdish)
+ */
+$.extend( $.validator.messages, {
+	required: "پێویستە ئەم خانەیە پڕ بکرێتەوە.",
+	remote: "تکایە ئەم خانەیە ڕاست بکەرەوە.",
+	email: "تکایە ئیمێلێکی تەواو داخل بکە.",
+	url: "تکایە ناونیشانێکی تەواو داخڵ بکە.",
+	date: "تکایە ڕێکەوتی درووست داخڵ بکە.",
+	dateFA: "تکایە ڕێکەوتێکی درووست داخڵ بکە.",
+	dateISO: "تکایە ڕێکەوتی درووست داخڵ بکە(ISO).",
+	number: "تکایە ژمارەی درووست داخل بکە.",
+	digits: "تکایە تەنها ژمارە داخڵ بکە.",
+	creditcard: "تکایە کرێدیت کاردی درووست داخڵ بکە.",
+	equalTo: "تکایە بڕی ڕاست داخڵ بکە.",
+	extension: "تکایە بڕێک داخڵ بکە کە",
+	alphanumeric: "تکایە بڕەکە بە ژمارەی ئینگلیزی داخڵ بکە.",
+	maxlength: $.validator.format( "تکایە زیاتر لە {٠} پیت داخڵ مەکە." ),
+	minlength: $.validator.format( "تکایە کەمترلە {٠} پیت داخڵ مەکە." ),
+	rangelength: $.validator.format( "تکایە بڕێک لە نێوان {٠} و {١} داخڵ بکە." ),
+	range: $.validator.format( "تکایە بڕێک لە نێوان {٠} و {١} پیت داخڵ بکە." ),
+	max: $.validator.format( "تکایە بڕێکی کەمتر لە {٠} داخڵ بکە." ),
+	min: $.validator.format( "تکایە بڕێکی زیاتر لە {٠} داخڵ بکە." ),
+	minWords: $.validator.format( "تکایە بەلایەنی کەم {٠} ووشە داخڵ بکە." ),
+	maxWords: $.validator.format( "تکایە زیاتر لە {٠} ووشە داخڵ مەکە." )
+} );
