fb609cf63bcb7539e941c8713238dcd8d86fe0b6
Kieran Brahney <kieran@supportpal.com>
2022-05-23 23:41:56 +0100

    Core: fix deprecated jquery .submit() event shorthand

--- src/core.js
+++ src/core.js
@@ -1113,7 +1113,7 @@ $.extend( $.validator, {
 			delete this.pending[ element.name ];
 			$( element ).removeClass( this.settings.pendingClass );
 			if ( valid && this.pendingRequest === 0 && this.formSubmitted && this.form() && this.pendingRequest === 0 ) {
-				$( this.currentForm ).submit();
+				$( this.currentForm ).trigger( 'submit' );
 
 				// Remove the hidden input that was used as a replacement for the
 				// missing submit button. The hidden input is added by `handle()`
