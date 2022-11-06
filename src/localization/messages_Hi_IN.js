b7be5e461bebff1540d109308d70105c4b11b41a
saurabhsharma2u <41580629+saurabhsharma2u@users.noreply.github.com>
2022-11-06 14:12:29 +0530

    Localisation: Update Hindi (India) translation

---
+++ src/localization/messages_Hi_IN.js
@@ -0,0 +1,27 @@
+/*
+ * Translated default messages for the jQuery validation plugin.
+ * Locale: Hi-IN (Hindi; हिंदी)
+ */
+$.extend( $.validator.messages, {
+	required: "आवश्यक फ़ील्ड।",
+	remote:"इस फ़ील्ड को जांचें।",
+	email: "एक वैध ई - मेल पते की प्रविष्टि करें।",
+	url: "कृपया एक वैध वेब पता दर्ज करें।",
+	date: "कृपया कोई मान्य दिनांक दर्ज करें।",
+	dateISO: "कृपया एक मान्य दिनांक (ISO) दर्ज करें।",
+	number: "कृपया सही अंक दर्ज करें।",
+	digits: "केवल संख्याएं दर्ज करें।",
+	creditcard: "कृपया एक वैध क्रेडिट कार्ड नंबर डालें।",
+	equalTo: "मेल नहीं खाता।",
+	extension: "कृपया एक मान्य एक्सटेंशन वाला मान दर्ज करें।",
+	maxlength: $.validator.format( "अधिक दर्ज न करें {0} वर्णों में से" ),
+	minlength: $.validator.format( "कृपया कम से कम {0} वर्ण दर्ज करें।" ),
+	rangelength: $.validator.format( "कृपया {0} और {1} वर्णों के बीच एक मान दर्ज करें।" ),
+	range: $.validator.format( "कृपया {0} और {1} के बीच एक मान दर्ज करें।" ),
+	max: $.validator.format( "कृपया {0} से कम या उसके बराबर मान दर्ज करें।" ),
+	min: $.validator.format( "कृपया {0} से अधिक या उसके बराबर मान दर्ज करें।" ),
+	nifES: "कृपया कोई मान्य NIF दर्ज करें।",
+	nieES: "कृपया एक मान्य NIE दर्ज करें।",
+	cifES: "कृपया एक मान्य CIF दर्ज करें.",
+	currency: "कृपया एक मान्य मुद्रा दर्ज करें।."
+} );
