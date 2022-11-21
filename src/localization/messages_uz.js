8b29c0c567bd05bdc3111addfb1d7ed8ce81f6ed
Nuriddin Islamov <nuriddin.k2003@gmail.com>
2022-11-21 14:25:22 +0000

    Add Uzbek language locale - Followed spacing rules - Implemented new language support - File named by ISO 639-1 format

---
+++ src/localization/messages_uz.js
@@ -0,0 +1,23 @@
+/*
+ * Translated default messages for the jQuery validation plugin.
+ * Locale: UZ (Uzbek; o'zbek tili)
+ */
+$.extend($.validator.messages, {
+	required: "Ushbu qator to'ldirilishi shart.",
+	remote: "Iltimos, to'g'ri qiymatni kiriting.",
+	email: "Iltimos, to'g'ri elektron pochta manzilini kiriting.",
+	url: "Iltimos, to'g'ri URL manzilini kiriting.",
+	date: "Iltioms, to'g'ri sana kiriting.",
+	dateISO: "Iltimos, to'g'ri sanani ISO formatida kiriting.",
+	number: "Iltimos, son kiriting.",
+	digits: "Iltimos, faqat raqamlar kiriting.",
+	creditcard: "Iltimos, bank kartani to'g'ri sonini kiriting.",
+	equalTo: "Iltimos, bir xil qiymatni yana kiriting.",
+	extension: "Iltimos, to'g'ri formatdagi faylni tanlang.",
+	maxlength: $.validator.format( "Iltimos, {0} dan ko'p belgilar kiritmang." ),
+	minlength: $.validator.format( "Iltimos, kamida {0} belgi kiriting." ),
+	rangelength: $.validator.format( "Iltimos, {0} dan {1} gacha uzunlikda bo'lgan qiymatni kiriting." ),
+	range: $.validator.format( "Iltimos, {0} dan {1} gacha bo'lgan son kiriting." ),
+	max: $.validator.format( "Iltimos, {0} dan kickik yoki teng bo'lgan son kiriting." ),
+	min: $.validator.format( "Iltimos, {0} dan katta yoki teng bo'lgan son kiriting." ),
+});
