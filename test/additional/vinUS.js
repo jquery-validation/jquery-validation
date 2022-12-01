278ff26399485f630b286f4f6bf9e3eb23cd25a1
Warren White <warren.white@vicpd.ca>
2022-11-30 16:31:23 -0800

    Additional: removed text license number, should be VIN

--- test/additional/vinUS.js
+++ test/additional/vinUS.js
@@ -6,6 +6,6 @@ QUnit.test( "vinUS", function( assert ) {
 	assert.ok( method( "LJCPCBLCX11000237" ), "Valid VIN with X check digit" );
 	assert.ok( !method( "LJCPCBLC011000237" ), "Invalid VIN with 0 check digit" );
 	assert.ok( !method( "2FTHF26F8" ), "InValid VIN  number" );
-	assert.ok( !method( "11111111X1111111" ), "Invalid test VIN license number" );
-	assert.ok( !method( "1111111101111111" ), "Invalid test VIN license number" );
+	assert.ok( !method( "11111111X1111111" ), "Invalid test VIN" );
+	assert.ok( !method( "1111111101111111" ), "Invalid test VIN" );
 } );
