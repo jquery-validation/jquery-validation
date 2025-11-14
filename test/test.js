e7e33a728f44dd4911bca9baa27fd65ae043a303
bytestream <kieran@supportpal.com>
2025-11-14 11:10:12 +0000

    Revert: "Tests: Add assertion to check form exists before validating"
    This reverts commit e3c28f79121e9e394e35c7a6f1655cd23c8d725b.

--- test/test.js
+++ test/test.js
@@ -370,11 +370,9 @@ QUnit.test( "Ignore elements that have form attribute set to other forms", funct
 } );
 
 QUnit.test( "Validate elements outside form with form attribute", function( assert ) {
-	assert.expect( 4 );
+	assert.expect( 3 );
 
 	var form = $( "#testForm29" );
-	assert.ok( form.length === 1, "Form should be found in the DOM" );
-
 	var v = form.validate();
 
 	// The form has one input inside and one input outside with form attribute
@@ -387,11 +385,9 @@ QUnit.test( "Validate elements outside form with form attribute", function( asse
 } );
 
 QUnit.test( "Validate checkboxes outside form with form attribute", function( assert ) {
-	assert.expect( 4 );
+	assert.expect( 3 );
 
 	var form = $( "#testForm30" );
-	assert.ok( form.length === 1, "Form should be found in the DOM" );
-
 	var v = form.validate();
 
 	// The form has one checkbox inside and one checkbox outside with form attribute
